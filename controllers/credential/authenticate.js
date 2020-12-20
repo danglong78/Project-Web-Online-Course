8dc0537e3dd513b3d0f6d7d6e187ec01a13ff926
hlhdaiaaii <hdaicenter@gmail.com>
2020-12-21 02:29:22 +0700

    add facebook signin

--- controllers/credential/authenticate.js
+++ controllers/credential/authenticate.js
@@ -110,33 +110,55 @@ const googleVerifyCb = async (req, accessToken, refreshToken, profile, done) => 
     }
 };
 
-// const facebookVerifyCb = async (req, accessToken, refreshToken, profile, done) => {
-//     // console.log(profile);
-//     User.findOne({ facebookID: profile.id }, async function (err, user) {
-//         if (err) {
-//             return done(err, false, req.flash('error', 'Something wrong happened. Try again!'));
-//         }
+const facebookVerifyCb = async (req, accessToken, refreshToken, profile, done) => {
+    // console.log(profile);
+    let err, credential;
 
-//         if (user) return done(null, user, req.flash('success', 'Welcome to EzTest'));
-//         else {
-//             const newUser = new User({
-//                 name: profile.displayName,
-//                 email: profile.emails[0].value,
-//                 password: profile.password || "",
-//                 facebookID: profile.id
-//             });
+    [err, credential] = await to(Credential.findOne({ googleID: profile.id }));
 
-//             try {
-//                 await newUser.save();
-//                 return done(null, newUser, req.flash('success', 'Welcome to EzTest'));
-//             }
-//             catch (e) {
-//                 return done(e, false);
-//             }
-//         };
-//     }
-//     )
-// };
+    if (err) {
+        return done(err, false, req.flash('error', 'Something wrong happened. Try again!'));
+    }
+
+    if (credential) return done(null, { id: credential.user, role: credential.role }, req.flash('success', 'WELCOME TO UDEMY CLONE'));
+    else {
+        // Try to find account having the same email as this google account
+        [err, credential] = await to(Credential.findOne({ email: profile.emails[0].value }));
+
+        if (err) {
+            return done(err, false, req.flash('error', 'Something wrong happened. Try again!'));
+        }
+
+        if (credential) {
+            return done(null, { id: credential.user, role: credential.role }, req.flash('success', 'WELCOME TO UDEMY CLONE'));
+        }
+        else {  // If still not found, create a new account
+            const newStudent = new Student({});
+            const newCredential = new Credential({
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                facebookID: profile.id,
+                role: 'Student',
+                user: newStudent._id
+            });
+
+            try {
+                await newStudent.save();
+                await newCredential.save();
+
+                let user = {
+                    id: newStudent._id,
+                    role: newCredential.role
+                }
+
+                return done(null, user, req.flash('success', 'WELCOME TO UDEMY CLONE'));
+            }
+            catch (err) {
+                return done(err, false, req.flash('error', 'Something wrong happened. Try again!'));
+            }
+        }
+    }
+};
 
 localStrategy = new LocalStrategy({
     usernameField: 'email',
@@ -151,13 +173,13 @@ googleStrategy = new GoogleStrategy({
     passReqToCallback: true
 }, googleVerifyCb);
 
-// facebookStrategy = new FacebookStrategy({
-//     clientID: "750118305860704",
-//     clientSecret: "c1cf6bc4928796e2e8d55df2c8ed5660",
-//     callbackURL: "http://localhost:3000/login/auth/facebook/callback",
-//     passReqToCallback: true,
-//     profileFields: ['id', 'emails', "displayName"],
-// }, facebookVerifyCb);
+facebookStrategy = new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    passReqToCallback: true,
+    profileFields: ['id', 'emails', "displayName"],
+}, facebookVerifyCb);
 
 module.exports.passportSetup = (app) => {
     app.use(passport.initialize());
@@ -165,5 +187,5 @@ module.exports.passportSetup = (app) => {
 
     passport.use(localStrategy);
     passport.use(googleStrategy);
-    // passport.use(facebookStrategy);
+    passport.use(facebookStrategy);
 }
--- routes/index.js
+++ routes/index.js
@@ -60,7 +60,20 @@ router.route('/auth/google')
   .get(passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.route('/auth/google/callback')
-  .get(passport.authenticate('google', { failureRedirect: "/login", failureFlash: true }), (req, res) => {
+  .get(passport.authenticate('google', { failureRedirect: "/signin", failureFlash: true }), (req, res) => {
+    if (req.user) {
+      const redirectUrl = "http://localhost:3000" + (req.session.redirectUrl || "/");
+      console.log("after authenticate");
+      console.log(redirectUrl);
+      res.redirect(redirectUrl);
+    }
+  });
+
+router.route('/auth/facebook')
+  .get(passport.authenticate('facebook', { scope: ['email'] }));
+
+router.route('/auth/facebook/callback')
+  .get(passport.authenticate('facebook', { failureRedirect: "/signin", failureFlash: true }), (req, res) => {
     if (req.user) {
       const redirectUrl = "http://localhost:3000" + (req.session.redirectUrl || "/");
       console.log("after authenticate");
