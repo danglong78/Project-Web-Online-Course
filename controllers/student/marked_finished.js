44841feb2429e347f46b5ca2a8005539a0473912
Tri Ho <18127236@student.hcmus.edu.vn>
2021-01-14 13:46:02 +0700

    [student]
    unmarked lecture is finished student controller

--- controllers/student/marked_finished.js
+++ controllers/student/marked_finished.js
@@ -16,4 +16,22 @@ const marked_finish = async function (studentID, courseID, lecutureID){
     return{success: true};
 }
 
-module.exports = marked_finish;
+const unMarked_finish = async function (studentID, courseID, lecutureID) {
+    var aUser = await Student.findById(studentID);
+    var index = aUser.courses.findIndex(c => c.course == courseID);
+    if (index < 0) {
+        return { success: false };
+    }
+    var lec_index = aUser.courses[index].progress.tracked_list.findIndex(tr => tr.lecture == lecutureID);
+    if (lec_index < 0) {
+        return { success: false };
+    }
+    aUser.courses[index].progress.tracked_list[lec_index].finished = false;
+    aUser.save().exec();
+    return { success: true };
+}
+
+module.exports = {
+    marked_finish,
+    unmarked_finish: unMarked_finish
+}
