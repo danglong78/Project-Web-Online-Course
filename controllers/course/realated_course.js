4e9959f53ab7ce1135387e5f283c87349afd3f94
Tri Ho <18127236@student.hcmus.edu.vn>
2021-01-14 19:15:55 +0700

    [course] add controller
    course controller get related course

---
+++ controllers/course/realated_course.js
@@ -0,0 +1,56 @@
+// const mongoosePaginate = require("mongoose-paginate-v2");
+const Course = require(__basedir + "/models/course").model;
+const SubCategory = require(__basedir + "/models/subCategory").model;
+
+const getRelatedCourses = async (courseID, catID, limit) => {
+    // console.log(catID);
+    //   const options = {
+    //     page,
+    //     limit,
+    //     sort: { enrollCount: -1 },
+    //     lean: true,
+    //   };
+
+    let courses;
+    try {
+        if (catID.length != 24) {
+            return;
+        }
+
+        const foundCat = await SubCategory.findOne({ _id: catID }).lean();
+        if (foundCat) {
+            // console.log("found cat");
+            courses = await Course.find({ subCategory: foundCat._id }).sort({ enrollCount: -1 }).lean();
+
+            courses.docs = await Course.populate(courses, [
+                {
+                    path: "lecturer",
+                    model: "Lecturer",
+                },
+                {
+                    path: "category",
+                    model: "MainCategory",
+                },
+                {
+                    path: "subCategory",
+                    model: "SubCategory",
+                },
+            ]);
+        }
+
+        // console.log(paginates);
+    } catch (e) {
+        console.log(e);
+    }
+    finally {
+        var result = [];
+        for (var i = 0; i < courses.length && result.length <= limit; i++) {
+            if (courses[i]._id != courseID) {
+                result.push(courses[i]);
+            }
+        }
+        return result;
+    }
+};
+
+module.exports = getRelatedCourses;
